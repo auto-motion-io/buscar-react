7501db6a29b170c07b0a88286acf42eefe2e27fb
Matheus Lima <matheus.slima@sptech.school>
2024-05-20 23:02:04 -0300

    tela meus serviços responsiva

--- buscar/src/pages/meusServicos/MeusServicos.jsx
+++ buscar/src/pages/meusServicos/MeusServicos.jsx
@@ -1,10 +1,21 @@
 import React from "react";
-import NavBar from "../../components/navbar/NavBar";
+import FormToken from "../../components/formToken/FormToken";
+import style from "./MeusServicos.module.css";
+import logo from "../../utils/assets/logo.svg";
 
 const MeusServicos = () => {
   return (
     <>
-        <NavBar currentPage={"meusServicos"} />
+    <div className="app-container">
+    <img src={logo} alt="Left" className="side-image left-image" />
+      <h1>Meus Serviços</h1>
+        <p>Informe o Token que sua oficina forneceu para visualização</p>
+        <div className={style["app"]}>
+          <div className={style["header"]}>
+          </div>
+          <FormToken />
+        </div>
+        </div>
     </>
 );
 };
